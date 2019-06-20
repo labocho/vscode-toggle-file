eba1db0a435d8ae1a79b236cd922f227ad83ca55
labocho <labocho@penguinlab.jp>
2019-06-20 14:48:07 +0900

    implement

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,28 @@
+import * as path from "path";
+import * as fs from "fs";
+
+interface IRule {
+	from: string;
+	to: string;
+}
+
+class Rule {
+	public from: RegExp;
+	public to: string;
+
+	constructor(config: IRule) {
+		this.from = new RegExp("^" + config.from + "$");
+		this.to = config.to;
+	}
+
+	match(path: string): boolean {
+		return path.match(this.from) !== null;
+	}
+
+	convert(path: string): string {
+		return path.replace(this.from, this.to);
+	}
+}
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -5,19 +30,55 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "toggle-file" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.toggleFile', () => {
+		const config = vscode.workspace.getConfiguration("toggleFile");
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const rules_config = <Array<IRule>>config.get("rules");
+		if (rules_config === undefined) { return; }
+		const rules = rules_config.map((e) => new Rule(e));
+
+		const editor = vscode.window.activeTextEditor;
+		if (editor === undefined) { return; }
+
+		const root = vscode.workspace.rootPath;
+		if (root === undefined) { return; }
+
+		const relative_path = path.relative(root, editor.document.fileName);
+
+		let convertedByfirstMatch: string | null = null;
+
+		for (const rule of rules) {
+			if (rule.match(relative_path)) {
+				const converted = path.join(root, rule.convert(relative_path));
+				if (convertedByfirstMatch === null) {
+					convertedByfirstMatch = converted;
+				}
+
+				if (fs.existsSync(converted)) {
+					vscode.workspace.openTextDocument(converted).then((doc) => {
+						vscode.window.showTextDocument(doc);
+					});
+					return;
+				}
+			}
+		}
+
+		if (convertedByfirstMatch !== null) {
+			vscode.window.showInputBox({prompt: "Create new file?", value: convertedByfirstMatch}).then((new_file) => {
+				if (new_file === undefined) { return; }
+				fs.writeFileSync(new_file, "");
+				vscode.workspace.openTextDocument(new_file).then((doc) => {
+					fs.unlinkSync(new_file);
+					vscode.window.showTextDocument(doc);
+				});
+			});
+			return;
+		}
+
+		vscode.window.showInformationMessage("Toggle file: Does not match any rule");
 	});
 
 	context.subscriptions.push(disposable);
